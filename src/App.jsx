418fd6473ca26283327e53ba4df9ee836a916616
Raven Hendricks <ravenkbeats@gmail.com>
2024-02-27 14:48:19 -0500

    mapped puppy names and rendered them onto screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,22 @@
-<div>
-</div>
+import { useState } from 'react'
+import { puppyList } from "./data.js"
+import './App.css'
+import './index.css'
+
+function App(){
+
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppyList);
+  
+  return (
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+          return <p key ={puppy.id}>{puppy.name}</p>;
+        })
+      }
+    </div>
+  );
+}
+
+export default App
